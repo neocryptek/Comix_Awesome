e2774478eb7fc8968a552f97eb322e6f83418488
Neocryptek <neocryptek@hotmail.com>
2010-01-17 16:31:59 -0800

    initial commit

---
+++ Awesome.lua
@@ -0,0 +1,8 @@
+
+--add slash command and call comix fail message parser
+SLASH_ComixAwesome1 = '/awesome'
+function SlashCmdList.ComixAwesome(msg, editbox)
+	local link = "\124cff71d5ff\124Hspell:58783\124h[Pure Awesome]\124h\124r"
+	msgSender(msg, link)
+end
+
